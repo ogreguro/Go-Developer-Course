5b6ed625982b5aebf279326cb8f98e803ef526e4
Aleksandr Gurov <a.gurov@automacon.ru>
2024-05-13 14:27:04 -0500

    commit

---
+++ excercise_10/10.1.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"first"
+	"fmt"
+)
+
+func main() {
+	fmt.Println(first.Hello())
+}
---
+++ excercise_10/10.3.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"first"
+	"fmt"
+	"second"
+)
+
+func main() {
+	fmt.Println(first.Hello())
+	fmt.Println(second.Hello())
+}
