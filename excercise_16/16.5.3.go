be5ec6ea416e37c74f6484317afb0f7bbb8026bd
Aleksandr Gurov <a.gurov@automacon.ru>
2024-06-21 14:18:12 -0500

    Create 16.5.3.go

---
+++ excercise_16/16.5.3.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+//доп вариант с использованием потокобезопасного доступа к общей переменной через мутексы
+
+func main() {
+	var wg sync.WaitGroup
+	var mu sync.RWMutex
+	rc := 10
+	stop := false
+
+	for i := 0; i < rc; i++ {
+		wg.Add(1)
+		go func(id int) {
+			defer wg.Done()
+			for {
+				time.Sleep(1 * time.Second)
+				fmt.Printf("сложные вычисления горутины: %d\n", id+1)
+				mu.RLock()
+				if stop {
+					mu.RUnlock()
+					fmt.Printf("stop горутина: %d\n", id+1)
+					return
+				}
+				mu.RUnlock()
+			}
+		}(i)
+	}
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		time.Sleep(3 * time.Second)
+		fmt.Println("ой, всё!")
+
+		mu.Lock()
+		stop = true
+		mu.Unlock()
+	}()
+
+	wg.Wait()
+}
