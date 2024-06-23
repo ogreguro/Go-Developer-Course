ef6e5f7179cf2cb4825cb45c9bef191ac766f6b0
Aleksandr Gurov <a.gurov@automacon.ru>
2024-06-23 15:11:51 -0500

    Version v1.0.0

---
+++ excercise_10_module/go.mod
@@ -0,0 +1,3 @@
+module github.com/ogreguro/excercise_10_module
+
+go 1.22.2
---
+++ excercise_10_module/main.go
@@ -0,0 +1,8 @@
+package excercise_10_module
+
+import "fmt"
+
+// Hello выводит сообщение в зависимости от версии модуля
+func Hello() {
+	fmt.Println("Hello, v1.0.0")
+}
