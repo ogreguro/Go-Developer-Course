02a67f009801558384956bee67b06da6083e446a
Aleksandr Gurov <a.gurov@automacon.ru>
2024-06-24 16:50:50 -0500

    commit

--- excercise_10/10.4.go
+++ excercise_10/10.4.go
@@ -1,1 +1,9 @@
 package main
+
+import (
+	"github.com/ogreguro/mod_10"
+)
+
+func main() {
+	mod_10.Hello()
+}
---
+++ excercise_10/go.mod
@@ -0,0 +1,5 @@
+module excercise_10
+
+go 1.22.2
+
+require github.com/ogreguro/mod_10 v1.0.0 // indirect
